a3bdd8a7c9037e4a7e00f2a802b3feaadd169f72
Oluwaseun Adedire <oluwaseun.adedire@gmail.com>
2020-07-02 00:43:02 +0100

    feat: add set and unset

--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
 import produce from 'immer';
 
-function isObject(value: any): boolean {
-  return typeof value === 'object' && !Array.isArray(value);
-}
-
 function getType<T>(value: T): PrimitiveType {
   const type = typeof value;
 
@@ -14,71 +10,140 @@ function getType<T>(value: T): PrimitiveType {
   return Array.isArray(value) ? 'array' : 'object';
 }
 
-function parsePatch<T>(target: T, source: PatchSpec<T>) {
+function parsePatch<T, P extends keyof T>(pack: PatchParams<T, P>) {
+  let {target, source, root, identifier} = pack;
   const keys = Object.keys(source);
 
   for (const key of keys) {
-    const value = source[key];
-    const type = getType(value);
+    if (key === '$') {
+      const type = getType(source[key]);
 
-    if (type === 'object' && Object.keys(value)?.[0] === '$') {
-      const targetValue = target[key];
-      const sourceValue = value['$'];
-      const targetType = getType(targetValue);
-      const sourceType = getType(sourceValue);
-
-      if (
-        targetType === sourceType &&
-        ['array', 'object'].includes(sourceType)
-      ) {
-        if (sourceType === 'array') {
-          target[key] = [...targetValue, ...sourceValue];
-          return;
-        }
-
-        if (sourceType === 'object') {
-          target[key] = {...targetValue, ...sourceValue};
-          return;
-        }
-      } else {
-        target[key] = sourceValue;
+      switch (type) {
+        case 'array':
+          Array.prototype.push.apply(root?.[identifier] ?? target, source[key]);
+          break;
+        case 'object':
+          for (const o in source[key]) {
+            (root?.[identifier] ?? target)[o] = source[key][o];
+          }
+          break;
+        default:
+          !!identifier ? (root[identifier] = source[key]) : false;
+          break;
       }
     } else {
-      parsePatch(target[key], value);
+      parsePatch({
+        target: target[key],
+        source: source[key],
+        root: target,
+        identifier: key,
+      });
+    }
+  }
+}
+
+function parseSet<T, P extends keyof T>(pack: PatchParams<T, P>) {
+  let {target, source, root, identifier} = pack;
+  const keys = Object.keys(source);
+
+  for (const key of keys) {
+    if (key === '$') {
+      if (!!identifier) {
+        root[identifier] = source[key];
+      } else {
+        const type = getType(source[key]);
+
+        switch (type) {
+          case 'array':
+            Array.prototype.splice.apply(target, [0, (target as any).length]);
+            Array.prototype.push.apply(target, source[key]);
+            break;
+          case 'object':
+            for (var o in target as any) {
+              delete target[o];
+            }
+
+            for (var o in source[key]) {
+              target[o] = source[key][o];
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    } else {
+      parseSet({
+        target: target[key],
+        source: source[key],
+        root: target,
+        identifier: key,
+      });
+    }
+  }
+}
+
+function parseUnset<T, P extends keyof T>(pack: PatchParams<T, P>) {
+  let {target, source, root, identifier} = pack;
+  const keys = Object.keys(source);
+
+  for (const key of keys) {
+    if (key === '$') {
+      const type = getType(root?.[identifier] ?? target);
+
+      switch (type) {
+        case 'array':
+          for (const index of source[key]) {
+            Array.prototype.splice.apply(root?.[identifier] ?? target, [
+              index,
+              1,
+            ]);
+          }
+          break;
+        case 'object':
+          for (const o of source[key]) {
+            delete (root?.[identifier] ?? target)[o];
+          }
+          break;
+        default:
+          break;
+      }
+    } else {
+      parseUnset({
+        target: target[key],
+        source: source[key],
+        root: target,
+        identifier: key,
+      });
     }
   }
 }
 
 function patch<T>(target: T, source: PatchSpec<T>) {
   const result = produce(target, (o) => {
-    parsePatch(o, source as any);
+    parsePatch({target: o, source: source as any});
   });
 
   return result;
 }
 
-function set<T>(target: T, source: SetSpec<T>) {}
+function set<T>(target: T, source: SetSpec<T>) {
+  const result = produce(target, (o) => {
+    parseSet({target: o, source: source as any});
+  });
 
-function unset<T>(target: T, source: UnsetSpec<T>) {}
+  return result;
+}
 
-const source = {a: 1, b: {a: 1, b: 2}};
+function unset<T>(target: T, source: SetSpec<T>) {
+  const result = produce(target, (o) => {
+    parseUnset({target: o, source: source as any});
+  });
 
-unset(source, {
-  b: {
-    $: ['a'],
-  },
-});
-
-// update(source, {b: {$set: {a: 1, b: 2, c: 3}}})
+  return result;
+}
 
 export default {
   patch,
   set,
   unset,
 };
-
-/*
-patcher.patch(a: {b: {$: 1}});
-patcher.unset(a: {b: {$: [1,2]}});
-patcher.set(a: {b: {$: 1}}); 
-*/
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -11,20 +11,33 @@ type PrimitiveType =
   | 'function'
   | 'array';
 
-type PatchSpec<T> = {
-  [K in keyof T]?:
-    | (T[K] extends object ? SetSpec<T[K]> : T[K])
-    | Commands<Partial<T[K]>>;
-};
+type PatchSpec<T> =
+  | {
+      [K in keyof T]?:
+        | (T[K] extends object ? SetSpec<T[K]> : T[K])
+        | Commands<Partial<T[K]>>;
+    }
+  | Commands<Partial<T>>;
 
-type SetSpec<T> = {
-  [K in keyof T]?:
-    | (T[K] extends object ? SetSpec<T[K]> : T[K])
-    | Commands<T[K]>;
-};
+type SetSpec<T> =
+  | {
+      [K in keyof T]?:
+        | (T[K] extends object ? SetSpec<T[K]> : T[K])
+        | Commands<T[K]>;
+    }
+  | Commands<Partial<T>>;
 
-type UnsetSpec<T> = {
-  [K in keyof T]?:
-    | (T[K] extends object ? UnsetSpec<T[K]> : T[K])
-    | Commands<ReadonlyArray<keyof T>>;
+type UnsetSpec<T> =
+  | {
+      [K in keyof T]?:
+        | (T[K] extends object ? UnsetSpec<T[K]> : T[K])
+        | Commands<ReadonlyArray<keyof T>>;
+    }
+  | Commands<ReadonlyArray<keyof T>>;
+
+type PatchParams<T, P extends keyof T> = {
+  target: T[P] | T;
+  source: PatchSpec<T | T[P]>;
+  root?: T;
+  identifier?: P;
 };
